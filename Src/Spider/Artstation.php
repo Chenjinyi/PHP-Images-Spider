3a054bd743edb5ebd45b79173dc6b224fe1dff8a
JinYi Chen <jinyi@JinYideMacBook-Pro.local>
2018-08-02 12:29:41 +0800

    Artstation Index Spider

--- Src/Spider/Artstation.php
+++ Src/Spider/Artstation.php
@@ -16,48 +16,80 @@ class Artstation
         "DNT:1",
         "Accept-Language:zh-CN,zh;q=0.8",
         "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36"
-    ];
+    ];//设置用户user-agent
 
     public $mode = [
-        'trending',
+        '1'=>'trending',
         'latest',
         'picks',
         'user',
-    ];
+    ];//可以选择的模式/可以调用的function
 
     public $spider_name = "Artstation";
 
+    /**
+     * 获取图片链接
+     * @param $result
+     * @param $spiderCore
+     * @return array 下载链接
+     */
+    public function get_img_url($result, $spiderCore)
+    {
+        $images_arr = [];
+        foreach ($result->data as $value) {
+            $images_url = $value->cover->medium_image_url;
+            $images_url = str_replace("medium", "large", $images_url);//生成更高清的图片下载地址
+            $file_name = $value->title . "-" . $value->cover_asset_id;//生成图片名
+            $file_name = $spiderCore->image_url_format($images_url, $file_name);
+            array_push($images_arr, [$file_name => $images_url]);
+        }
+        return $images_arr;
+    }
+
+    public function index_spider_core($spiderCore,$spider_name)
+    {
+        $posts_num = $spiderCore->user_input("请输入爬取页数(1页=50个作品)/(默认为1):", 1);
+        for ($start_num = 1; $start_num <= $posts_num; $start_num++) {
+            $url = "https://www.artstation.com/projects.json?page=" . $start_num . "&sorting=" . $spider_name;
+            $result = $spiderCore->curl_get($url, $this->userAgent);
+            $result = json_decode($result);
+            $images_arr = $this->get_img_url($result, $spiderCore);
+            $spiderCore->quick_down_img($this->spider_name . "-" . $spider_name, $images_arr);
+
+        }
+    }
+
+    /**
+     * 指定用户爬取
+     * @param $spiderCore
+     */
     public function user($spiderCore)
     {
         @empty($user = $spiderCore->user_input("请输入你要抓取的用户ID（https://www.artstation.com/xxxxx）：", null)) ? die(PHP_EOL . "用户不能为空") : print_r(PHP_EOL . "开始获取图片" . PHP_EOL);
         $result = $spiderCore->curl_get("https://www.artstation.com/users/" . $user . "/projects.json", $this->userAgent); //爬取json
         $result = json_decode($result);
 
-        $images_arr = [];
-        //获取图片下载地址
-        foreach ($result->data as $value) {
-            $images_url = $value->cover->medium_image_url;
-            $images_url = str_replace("medium", "large", $images_url);//生成更高清的图片下载地址
-            $file_name = $value->title . "-" . $value->cover_asset_id . "-" . $user;//生成图片名
-            $file_name = $spiderCore->image_url_format($images_url, $file_name);
-            array_push($images_arr, [$file_name => $images_url]);
-        }
+        $images_arr = $this->get_img_url($result, $spiderCore);
         $spiderCore->quick_down_img($this->spider_name . "-" . $user, $images_arr);
     }
 
+    /**
+     * 最新图片爬取
+     * @param $spiderCore
+     */
     public function latest($spiderCore)
     {
-
+        $this->index_spider_core($spiderCore,'latest');
     }
 
     public function picks($spiderCore)
     {
-
+        $this->index_spider_core($spiderCore,'picks');
     }
 
     public function trending($spiderCore)
     {
-
+        $this->index_spider_core($spiderCore,'trending');
     }
 
     public function search($spiderCore)
